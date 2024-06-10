bee6489ebe743405788317dc81ed48ea4147d590
Niels van den Broek <123738989+NielsVDBroek@users.noreply.github.com>
2024-06-10 13:18:04 +0200

    Revert "Input fields"

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import { useEffect, useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.scss'
-import { Auth } from './components/auth/auth'
-import { db } from './config/firebase'
-import { getDocs, collection, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore'
-import MainHeader from './components/mainHeader/MainHeader'
-import Posts from './components/posts/Posts'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <MainHeader/>
-      <main>
-        <Routes>
-          <Route path="/" element={<Posts/>}/>
-          <Route path="/login" element={<Auth/>}/>
-        </Routes>
-      </main>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
 export default App
