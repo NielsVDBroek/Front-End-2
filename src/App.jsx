18ddb0682cd12c8cb460b5c983520cfca750f5b0
NielsVDBroek <Niels.BroekVanDen@gmail.com>
2024-06-10 13:29:39 +0200

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.scss'
 import { Auth } from './components/auth/auth'
-import { db } from './config/firebase'
-import { getDocs, collection, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore'
 import MainHeader from './components/mainHeader/MainHeader'
 import Posts from './components/posts/Posts'
 import Account from './components/account/Account';
