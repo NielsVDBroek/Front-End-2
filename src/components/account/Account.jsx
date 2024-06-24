98934c32020b4b7209f2c3f47e783aaa4ff81ee2
nick <nickswanenberg@gmail.com>
2024-06-24 15:25:58 +0200

    Account page verbeterd

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -3,10 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { auth } from "../../config/firebase";
 import { signOut } from "firebase/auth";
 import { Link } from "react-router-dom";
+import './Account.scss';
+
 
 function Account() {
   const navigate = useNavigate();
   const [currentUser, setCurrentUser] = useState(null);
+  
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -29,14 +32,26 @@ function Account() {
   };
 
   return(
-    <div>
-      <div>
-        Username: {currentUser?.displayName}
+    <div className="Account-container">
+      <div className="Container-user">
+        <div className="back-bar">
+          <button className="Back-btn" onClick={() => navigate(-1)}>&#129028;</button>
+          {currentUser?.displayName}
+        </div>
+        <div className="Banner">Banner</div>
+        <div className="Userinfo">
+          <div className="Username">
+            {currentUser?.displayName}
+          </div>
+          <div className="Email">
+            {currentUser?.email}
+          </div>
+        </div>
+        <button className="Logout-btn" onClick={logout}>Logout</button>
       </div>
-      <div>
-        Email: {currentUser?.email}
+      <div className="Container-posts">
+        //laat hier de posts van de gebruiker zien
       </div>
-      <button onClick={logout}>Logout</button>
     </div>
   );
 }
