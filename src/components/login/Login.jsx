41d247a4ed515afb0a01966458354c9e594dcb14
nick <nickswanenberg@gmail.com>
2024-06-26 17:19:19 +0200

    Layout login/register

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { auth, googleProvider } from "../../config/firebase";
-import {getAuth, signInWithEmailAndPassword, signInWithPopup,} from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, } from "firebase/auth";
 import './Login.scss';
 
 const Login = () => {
@@ -38,17 +38,61 @@ const Login = () => {
     <>
       <main>
         <div className="container-Login">
-        <button className="Login-button-option" onClick={signInWithGoogle}>Sign in with Google</button>
-        <div className="Divider-login-buttons-container">
-          <div className="Divider-login-buttons"></div>
-          <p>of</p>
-          <div className="Divider-login-buttons"></div>
-        </div>
-        <button className="Login-button-option" onClick={() => navigate("/register")}>Account aanmaken</button>
-        <div>
-          <h3>Heb je al een account?</h3>
-        </div>
-          <form>
+          <div className="Login-account-button">
+            <button className="Login-button-option" onClick={signInWithGoogle}>Sign in with Google</button>
+            <div className="Divider-login-buttons-container">
+              <div className="Divider-login-buttons"></div>
+              <p>of</p>
+              <div className="Divider-login-buttons"></div>
+            </div>
+            <button className="Login-button-option" onClick={() => navigate("/register")}>Account aanmaken</button>
+          </div>
+          <div className="account-login Login-account-button">
+            <h4>Heb je al een account?</h4>
+
+            <button className="Login-button-option" onClick={() => document.getElementById('my_modal_2').showModal()}>Inloggen</button>
+            <dialog id="my_modal_2" className="modal">
+              <form method="dialog">
+                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+              </form>
+              <div className="modal-box">
+
+                <h3 className="font-bold text-lg Inloggen-modal">Inloggen</h3>
+                <div className="modal-action">
+                  <form method="dialog" className='post-form'>
+                    <div className="Login-input-fields">
+                      <label htmlFor="email-address">Email address</label>
+                      <input
+                        id="email-address"
+                        name="email"
+                        type="email"
+                        required
+                        placeholder="Email address"
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
+
+                    <div className="Login-input-fields">
+                      <label htmlFor="password">Password</label>
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        required
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </div>
+
+                    <div className="Login-modal-buttons">
+                      <button onClick={onLogin}>Login</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </dialog>
+          </div>
+          {/* <form>
             <div>
               <label htmlFor="email-address">Email address</label>
               <input
@@ -76,7 +120,7 @@ const Login = () => {
             <div>
               <button onClick={onLogin}>Login</button>
             </div>
-          </form>
+          </form> */}
         </div>
       </main>
     </>
